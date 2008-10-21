ffdfc359ed68bff807d70ca46691df373a65f861
Andrew Miller <ak.miller@auckland.ac.nz>
2008-10-21 16:55:09 +1300

    Optimise sign test log_2 pvalue instead of difference between datasets.

--- FindOptimalSVMParameters.cpp
+++ FindOptimalSVMParameters.cpp
@@ -12,14 +12,93 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <eo/apply.h>
+#include <math.h>
 
 namespace po = boost::program_options;
 namespace fs = boost::filesystem;
 
 typedef eoReal<eoMinimizingFitness> Indi;
 
+class ResultListener
+{
+public:
+  ResultListener(uint32_t anArrays, uint32_t anGenes)
+    : mnArrays(anArrays), mnGenes(anGenes), mData(NULL)
+  {
+    mData = new double[mnArrays * mnGenes * 2];
+    p = mData;
+  }
+
+  ~ResultListener()
+  {
+    if (mData)
+      delete [] mData;
+  }
+
+  void startRow(uint32_t aIdx)
+  {
+  }
+
+  void endRow(uint32_t aIdx)
+  {
+  }
+
+  void result(uint32_t gene, double val)
+  {
+    *p++ = val;
+  }
+
+  double log2pval()
+  {
+    double * p1 = mData, * p2 = mData + mnGenes * mnArrays;
+    double * end = p2;
+
+    int32_t n = 0, x = 0;
+
+    while (p1 < end)
+    {
+      double v1 = *p1++, v2 = *p2++;
+
+      if (finite(v1) && finite(v2) && v1 != v2)
+      {
+        n++;
+        if (v1 > v2)
+          x++;
+      }
+    }
+
+    if (x > n - x)
+      x = n - x;
+
+    return ((-n) + 1.0 + recurseComputeLogProb(n, 1, x));
+  }
+
+private:
+  uint32_t mnArrays, mnGenes;
+  double * mData, * p;
+
+  double recurseComputeLogProb(uint32_t n, uint32_t i, uint32_t x)
+  {
+    if (i > x)
+      return 0.0;
+
+    return addOneToExponential(log2((n + 1 - i) / (0.0 + i)) +
+                               recurseComputeLogProb(n, i + 1, x));
+  }
+
+  double addOneToExponential(double v)
+  {
+    // Approximation: for large v, log_2(2^v + 1) approx v
+    if (v > 30.0)
+      return v;
+
+    return log2(1.0 + pow(2.0, v));
+  }
+};
+
 double
-safe_svm_evaluator(GRNModel& m, const std::list<std::string>& testingSet)
+safe_svm_evaluator(GRNModel& m, GRNModel& nm, const std::list<std::string>& testingSet,
+                   uint32_t numGenes, uint32_t numArrays)
 {
   int pipes[2];
   pid_t pid;
@@ -29,8 +108,21 @@ safe_svm_evaluator(GRNModel& m, const std::list<std::string>& testingSet)
   {
     close(pipes[0]);
 
+    ResultListener rl(numGenes, numArrays);
+
     m.trainSVMs();
-    double result = m.testSVMs(testingSet);
+
+    m.testSVMs(testingSet, rl);
+    printf("Model training done.\n");
+    double x = 0.0;
+    write(pipes[1], &x, sizeof(double));
+
+    nm.trainSVMs();
+
+    nm.testSVMs(testingSet, rl);
+    
+    double result = rl.log2pval();
+    printf("Null model training done; log_2 p %g\n", result);
     write(pipes[1], &result, sizeof(double));
 
     exit(0);
@@ -40,7 +132,7 @@ safe_svm_evaluator(GRNModel& m, const std::list<std::string>& testingSet)
     close(pipes[1]);
 
     struct timeval tv;
-    // Give it 10 minutes...
+    // Give it 10 minutes (for each result, 20 total)...
     tv.tv_sec = 600;
     tv.tv_usec = 0;
     fd_set s;
@@ -49,30 +141,54 @@ safe_svm_evaluator(GRNModel& m, const std::list<std::string>& testingSet)
     double result;
     if (select(pipes[0] + 1, &s, NULL, &s, &tv) <= 0)
     {
+      printf("Timeout on model; killing process.\n");
       kill(pid, SIGKILL);
-      result = 1E60;
+      result = 0;
     }
     else
     {
-      if (read(pipes[0], &result, sizeof(double)) != sizeof(double))
-        result = 1E60;
+      if (read(pipes[0], &result, sizeof(double) + 1) != sizeof(double))
+      {
+        printf("Read error on model.\n");
+        result = 0;
+      }
+      else
+      {
+        tv.tv_sec = 600;
+        tv.tv_usec = 0;
+        FD_SET(pipes[0], &s);
+        if (select(pipes[0] + 1, &s, NULL, &s, &tv) <= 0)
+        {
+          printf("Timeout on null model.\n");
+          kill(pid, SIGKILL);
+          result = 0;
+        }
+        else if (read(pipes[0], &result, sizeof(double) + 1) != sizeof(double))
+        {
+          printf("Read error on null model.\n");
+          result = 0;
+        }
+      }
     }
     close(pipes[0]);
     int status;
     wait(&status);
 
+    printf("Returning result: %g\n", result);
+
     return result;
   }
-  return 1E60;
+  return 0;
 }
 
 class EvaluateSVMFit
   : public eoEvalFunc<Indi>
 {
 public:
-  EvaluateSVMFit(GRNModel& aM, const std::list<std::string>& aTestingSet,
-                 std::list<double>& aLastRun)
-    : mM(aM), mTestingSet(aTestingSet), mLastRun(aLastRun)
+  EvaluateSVMFit(GRNModel& aM, GRNModel& aNM, const std::list<std::string>& aTestingSet,
+                 std::list<double>& aLastRun, uint32_t aNumGenes, uint32_t aNumArrays)
+    : mM(aM), mNM(aNM), mTestingSet(aTestingSet), mLastRun(aLastRun),
+      mNumGenes(aNumGenes), mNumArrays(aNumArrays)
   {
   }
   
@@ -82,6 +198,7 @@ public:
     {
       double gamma = exp(aIndi[0]), C = exp(aIndi[1]), p = exp(aIndi[2]);
       mM.setSVMParameters(gamma, C, p);
+      mNM.setSVMParameters(gamma, C, p);
 
       double fitness;
       if (!mLastRun.empty())
@@ -90,7 +207,7 @@ public:
         mLastRun.pop_front();
       }
       else
-        fitness = safe_svm_evaluator(mM, mTestingSet);
+        fitness = safe_svm_evaluator(mM, mNM, mTestingSet, mNumGenes, mNumArrays);
       aIndi.fitness(fitness);
       std::cout << "SVM Result: log2 gamma (" << aIndi[0] << ") "
                    "log2 C (" << aIndi[1] << ") log2 p (" << aIndi[2]
@@ -100,9 +217,10 @@ public:
   }
 
 private:
-  GRNModel& mM;
+  GRNModel& mM, & mNM;
   const std::list<std::string>& mTestingSet;
   std::list<double>& mLastRun;
+  uint32_t mNumGenes, mNumArrays;
 };
 
 int
@@ -127,13 +245,15 @@ main(int argc, char** argv)
   const unsigned int SEED = 42;	// seed for random number generator
 
   po::options_description desc;
-  std::string matrixdir, model, trainingset, testingset, lastrun;
+  std::string matrixdir, model, nullmodel, trainingset, testingset, lastrun;
 
   desc.add_options()
     ("matrixdir", po::value<std::string>(&matrixdir),
      "The directory set up by SOFT2Matrix")
     ("model", po::value<std::string>(&model),
      "The gene regulatory network model")
+    ("nullmodel", po::value<std::string>(&nullmodel),
+     "The gene regulatory network null (scrambled) model")
     ("trainingset", po::value<std::string>(&trainingset),
      "The list of arrays which have been selected for inclusion in the training set")
     ("testingset", po::value<std::string>(&testingset),
@@ -158,6 +278,8 @@ main(int argc, char** argv)
       wrong = "trainingset";
     else if (!vm.count("testingset"))
       wrong = "testingset";
+    else if (!vm.count("nullmodel"))
+      wrong = "nullmodel";
   }
 
   if (wrong != "")
@@ -178,6 +300,12 @@ main(int argc, char** argv)
   if (!fs::is_regular(model))
   {
     std::cout << "Model file doesn't exist or not regular file."
+              << std::endl;
+    return 1;
+  }
+  if (!fs::is_regular(nullmodel))
+  {
+    std::cout << "Null model file doesn't exist or not regular file."
               << std::endl;
     return 1;
   }
@@ -218,16 +346,20 @@ main(int argc, char** argv)
 
   ExpressionMatrixProcessor emp(matrixdir);
   GRNModel m(model, emp, 50);
+  GRNModel m2(nullmodel, emp, 50);
 
   std::list<std::string> trainingArrays, testingArrays;
   m.loadArraySet(trainingset, trainingArrays);
   m.loadArraySet(testingset, testingArrays);
   m.loadSVMTrainingData(trainingArrays);
+  m2.loadArraySet(trainingset, trainingArrays);
+  m2.loadArraySet(testingset, testingArrays);
+  m2.loadSVMTrainingData(trainingArrays);
 
   // We seed it just so we can restart if need be.
   rng.reseed(SEED);
 
-  EvaluateSVMFit eval(m, trainingArrays, lastRun);
+  EvaluateSVMFit eval(m, m2, trainingArrays, lastRun, 50, emp.getNumArrays());
   eoUniformGenerator<double> uGen(-15.0, 15.0);
   eoInitFixedLength<Indi> random(VEC_SIZE, uGen);
   eoPop<Indi> pop(POP_SIZE, random);
@@ -252,10 +384,8 @@ main(int argc, char** argv)
 
   eoGenContinue<Indi> genCont(MAX_GEN);
   eoSteadyFitContinue<Indi> steadyCont(MIN_GEN, STEADY_GEN);
-  eoFitContinue<Indi> fitCont(0);
   eoCombinedContinue<Indi> continuator(genCont);
   continuator.add(steadyCont);
-  continuator.add(fitCont);
 
   eoSGATransform<Indi> transform(xover, P_CROSS, mutation, P_MUT);
   eoEasyEA<Indi> gga(continuator, eval, select, transform, replace);
